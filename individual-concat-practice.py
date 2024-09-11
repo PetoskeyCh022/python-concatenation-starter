1fb428ea82ef86fac8f544540343b6ad6a6e06d3
PetoskeyCh022 <PetoskeyCh022@northwested.org>
2024-09-11 10:42:02 -0400

    update starter files

--- individual-concat-practice.py
+++ individual-concat-practice.py
@@ -4,7 +4,9 @@
 
 # Part 1
 # Use concatenation to build and display a string that displays which city and state you live in
-
+#city = 'Traverse city'
+#state = 'Michigan'
+#print('I live in ' + city + ', ' + state + '.')
 
 
 
@@ -13,7 +15,9 @@
 # Create one variable to store the user's first name
 # Create another variable that stores the welcome message
 # Use concatenation to create the customized message
-
+#first_name = input('What is your first name?: ')
+#welcome_msg = 'Welcome to Jumanji, ' + first_name
+#print(welcome_msg)
 
 
 
@@ -21,7 +25,8 @@
 # Assign a number to your age variable
 # Use the built-in Python str( ) function to convert your age to a string (when doing concatenation)
 # Use concatenation to display a sentence that tells us your first name and age
-
+#age = 17
+#print('My age is ' + str(age) + '.')
 
 
 
@@ -29,3 +34,7 @@
 
 # Part 4
 # Use an f-string to build and display a string that contains your first name, last name, and your height in inches
+#first_name = 'Charles'
+#last_name = 'Petoskey'
+#height = '67 inches'
+#print(f'My first name is {first_name} and my last name is {last_name}, and my height is {height}.')
--- paired-concat-practice.py
+++ paired-concat-practice.py
@@ -9,20 +9,29 @@
 # Part 1
 # Create variables to store your first, middle, and last name
 # Have Python print a message that contains your concatenated full name, i.e., your combined first, middle and last names
-
+#first_name = 'Charles'
+#middle_name = 'James'
+#last_name = 'Petoskey'
+#print(first_name + ' ' + middle_name + ' ' + last_name + '.')
 
 
 # Part 2
 # Assume you're building a Space Invaders game
 # Use concatenation to create and display a custom welcome message that includes the player's first name
-
+#player_name = input('players name:')
+#print('Welcome ' + player_name + ' to Space Invaders.')
 
 
 # Part 3
 # Use concatenation to build and display a string that includes your first name, last name, and the year you were born
-
+#first_name = 'Charles'
+#last_name = 'Petoskey'
+#birth_year = 2007
+#print(first_name + ' ' + last_name + ' was born in ' + str(birth_year) + '.')
 
 
 # Part 4
 # Use concatenation to create and display a sentence that says which country in the world
 # you would visit if money were no object
+#country = 'Chad'
+#print('If money was no thing I would go to ' + country)
